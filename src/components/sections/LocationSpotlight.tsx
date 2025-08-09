adaf0c61c2361298306d9dbdace08f1f00117925
Deo Kalule <deo.kalule@immertec.com>
2025-08-09 07:28:43 -0700

    feat(location): update Carlsbad contact and exterior photo

--- src/components/sections/LocationSpotlight.tsx
+++ src/components/sections/LocationSpotlight.tsx
@@ -12,7 +12,7 @@ function LocationSpotlight() {
           <div className="w-layout-grid grid-2-columns location-image-right">
             {/* Content card */}
             <div className="card location-card-content-side">
-              <h2 className="display-9">New York, NY</h2>
+              <h2 className="display-9">Mukyala Day Spa – Carlsbad Village</h2>
 
               <div className="mg-top-40px">
                 <div className="grid-1-column gap-row-20px">
@@ -21,13 +21,13 @@ function LocationSpotlight() {
                     <div className="decoration-dot bullet-point mg-right-14px" />
                     <div className="inner-container _298px">
                       <a
-                        href="https://www.google.com/maps/place/55+East+Birchwood+Ave,+Brooklyn,+New+York+11201"
+                        href="https://www.google.com/maps/place/2951+State+St,+Carlsbad,+CA+92008"
                         target="_blank"
                         rel="noreferrer noopener"
                         className="location-info-link w-inline-block"
                       >
                         <div className="paragraph-large">
-                          55 East Birchwood Ave. Brooklyn, New York 11201, United States
+                          2951 State Street, Carlsbad, CA 92008, United States
                         </div>
                       </a>
                     </div>
@@ -36,19 +36,16 @@ function LocationSpotlight() {
                   {/* Phone */}
                   <div className="flex-horizontal bullet-point-left">
                     <div className="decoration-dot bullet-point mg-right-14px" />
-                    <a href="tel:+14878701087" className="location-info-link w-inline-block">
-                      <div className="paragraph-large">(487) 870 - 1087</div>
+                    <a href="tel:+17608701087" className="location-info-link w-inline-block">
+                      <div className="paragraph-large">(760) 870 1087</div>
                     </a>
                   </div>
 
                   {/* Email */}
                   <div className="flex-horizontal bullet-point-left">
                     <div className="decoration-dot bullet-point mg-right-14px" />
-                    <a
-                      href="mailto:newyork@salon.com"
-                      className="location-info-link w-inline-block"
-                    >
-                      <div className="paragraph-large">newyork@salon.com</div>
+                    <a href="mailto:info@mukyala.com" className="location-info-link w-inline-block">
+                      <div className="paragraph-large">info@mukyala.com</div>
                     </a>
                   </div>
 
@@ -56,8 +53,8 @@ function LocationSpotlight() {
                   <div className="flex-horizontal bullet-point-left">
                     <div className="decoration-dot bullet-point mg-right-14px" />
                     <div className="flex gap-column-28px---row-4px children-wrap">
-                      <div className="paragraph-large">Mon - Fri: 10am - 6pm</div>
-                      <div className="paragraph-large">Sat - Sun: 10am - 6pm</div>
+                      <div className="paragraph-large">Mon to Fri: 10 am to 6 pm</div>
+                      <div className="paragraph-large">Sat and Sun: 10 am to 6 pm</div>
                     </div>
                   </div>
                 </div>
@@ -67,10 +64,10 @@ function LocationSpotlight() {
             {/* Image */}
             <div className="image-wrapper border-radius-20px">
               <img
-                src="/images/new-york-location-short-image-hair-x-webflow-template.jpg"
-                srcSet="/images/new-york-location-short-image-hair-x-webflow-template-p-500.jpg 500w, /images/new-york-location-short-image-hair-x-webflow-template-p-800.jpg 800w, /images/new-york-location-short-image-hair-x-webflow-template.jpg 1480w"
+                src="/images/carlsbad-location-exterior.jpg"
+                srcSet="/images/carlsbad-location-exterior-p-500.jpg 500w, /images/carlsbad-location-exterior-p-800.jpg 800w, /images/carlsbad-location-exterior.jpg 1480w"
                 sizes="(max-width: 479px) 92vw, (max-width: 991px) 100vw, (max-width: 1439px) 57vw, 58vw"
-                alt="New York spa location exterior"
+                alt="Carlsbad spa location exterior"
                 className="_w-h-100 fit-cover"
                 loading="lazy"
               />
