c86e51610374f6f02764846e97f138ed95884773
Deo Kalule <deo.kalule@immertec.com>
2025-08-09 07:30:03 -0700

    refactor(footer): remove CTA/newsletter, center branding, and use PNG logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import logoSrc from '/images/logo-web-hair-x-webflow-template.svg';
+import logoSrc from '/images/mukyala_logo.png';
 
 function Footer() {
   return (
@@ -6,30 +6,9 @@ function Footer() {
       <div className="w-layout-blockcontainer container-default w-container">
         <div className="card footer-card">
           <div className="inner-container _1140px">
-            <div className="cta-footer-card">
-              <div className="w-layout-grid grid-2-columns cta-footer-card-image-right">
-                <div>
-                  <h2 className="display-8 mg-bottom-32px">
-                    Ready for a revolutionary change of look?
-                    <br />
-                    We are here to make you shine like never before.
-                  </h2>
-                  <div className="buttons-row left wrap">
-                    <a href="/reservation" className="button-primary large w-inline-block">
-                      <div className="text-block">Make a reservation</div>
-                    </a>
-                    <a href="/services" className="link center-mbp w-inline-block">
-                      <div>Browse services</div>
-                      <div className="item-icon-right medium">
-                        <div className="icon-font-rounded"></div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {/* CTA footer card removed as per design update */}
 
-            <div className="footer-middle mg-top-64px">
+            <div className="footer-middle">
               <div className="footer-logo---newsletter">
                 <div>
                   <a href="/" className="logo-link w-inline-block">
@@ -40,36 +19,7 @@ function Footer() {
                   </div>
                 </div>
 
-                <div className="mg-top-40px">
-                  <form
-                    className="newsletter-form"
-                    onSubmit={(e) => {
-                      e.preventDefault();
-
-                      alert('Thanks for subscribing!');
-                    }}
-                  >
-                    <label htmlFor="email-footer" className="mg-bottom-16px">
-                      Subscribe to our newsletter!
-                    </label>
-                    <div className="input-wrapper">
-                      <input
-                        id="email-footer"
-                        name="email"
-                        type="email"
-                        placeholder="Enter your email"
-                        required
-                        className="input-line medium w-input"
-                      />
-                      <button
-                        type="submit"
-                        className="button-primary small inside-line-input medium w-button"
-                      >
-                        Subscribe
-                      </button>
-                    </div>
-                  </form>
-                </div>
+                {/* Newsletter signup removed per design update */}
               </div>
             </div>
           </div>
